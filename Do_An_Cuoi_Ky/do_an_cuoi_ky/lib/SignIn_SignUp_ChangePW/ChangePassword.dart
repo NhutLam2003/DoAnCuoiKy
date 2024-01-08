b192677690e132072d4f0f0bb715040cccb8b1fc
HuynhThienTam <huynhtam20202003@gmail.com>
2024-01-08 20:54:46 +0700

    layout

---
+++ Do_An_Cuoi_Ky/do_an_cuoi_ky/lib/SignIn_SignUp_ChangePW/ChangePassword.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+class ChangePassword extends StatefulWidget {
+  const ChangePassword({super.key});
+
+  @override
+  State<ChangePassword> createState() => _ChangePasswordState();
+}
+
+class _ChangePasswordState extends State<ChangePassword> {
+  @override
+  Widget build(BuildContext context) {
+    // final FirebaseAuth _auth = FirebaseAuth.instance;
+    // final User? currentUser = _auth.currentUser;
+
+    // String password = SignIn.pass;
+
+    var passOld = TextEditingController();
+    var passNew = TextEditingController();
+    var confirmPassNew = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Quên mật khẩu"),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.popAndPushNamed(context, '/home');
+            },
+            icon: const Icon(Icons.arrow_back)),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: TextField(
+              controller: passOld,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                  label: const Text("Mật khẩu cũ"),
+                  prefixIcon: const Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30))),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: TextField(
+              controller: passNew,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                  label: const Text("Mật khẩu mới"),
+                  prefixIcon: const Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30))),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: TextField(
+              controller: confirmPassNew,
+              decoration: InputDecoration(
+                  label: const Text("Nhập lại mật khẩu"),
+                  prefixIcon: const Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30))),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: TextButton(
+              onPressed: () async {
+                if (passNew.text.isEmpty ||
+                    confirmPassNew.text.isEmpty ||
+                    passOld.text.isEmpty) {
+                  showalert(context, "Thông tin mật khẩu không được để trống",
+                      () {
+                    Navigator.pop(context);
+                  });
+                  // } else if (passOld.text != password) {
+                  //   print(password);
+                  showalert(context, "Thông tin mật khẩu cũ không đúng", () {
+                    Navigator.pop(context);
+                  });
+                } else if (passNew.text != confirmPassNew.text) {
+                  showalert(context,
+                      "Mật khẩu mới và nhập lại mật khẩu không trùng khớp", () {
+                    Navigator.pop(context);
+                  });
+                } else {
+                  try {
+                    // await currentUser?.updatePassword(passNew.text);
+                    showalert(context,
+                        "Đổi mật khẩu thành công vui lòng đăng nhập lại", () {
+                      Navigator.popAndPushNamed(context, '/signin');
+                    });
+                    // Navigator.popAndPushNamed(context, '/Signin');
+                  } catch (e) {
+                    showalert(context, "Đổi mật khẩu thất bại", () {
+                      Navigator.pop(context);
+                    });
+                  }
+                }
+              },
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.blue),
+              ),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width / 2,
+                child: const Text(
+                  "Cập nhật",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+showalert(BuildContext context, String content, void Function() callback) {
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(
+            "Thông báo",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  callback();
+                },
+                child: const Text('OK')),
+          ],
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.warning, size: 70),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: Text(content),
+              ),
+            ],
+          ),
+        );
+      });
+}
---
+++ Do_An_Cuoi_Ky/do_an_cuoi_ky/lib/SignIn_SignUp_ChangePW/SignIn.dart
@@ -0,0 +1,229 @@
+//import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({super.key});
+  static String pass = "";
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  // Future<UserCredential> signInWithGoogle() async {
+  //   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  //   final GoogleSignInAuthentication? googleAuth =
+  //       await googleUser?.authentication;
+  //   final credential = GoogleAuthProvider.credential(
+  //     accessToken: googleAuth?.accessToken,
+  //     idToken: googleAuth?.idToken,
+  //   );
+  //   return await FirebaseAuth.instance.signInWithCredential(credential);
+  // }
+  final form_key = GlobalKey<FormState>();
+  String txt = "";
+  final email = TextEditingController();
+  final password = TextEditingController();
+  bool obs = true;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Form(
+          key: form_key,
+          child: SingleChildScrollView(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(top: 80, left: 20),
+                child: const Text(
+                  "ĐĂNG NHẬP",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 30),
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: TextFormField(
+                  controller: email,
+                  
+                  decoration: const InputDecoration(
+                    suffixIcon: Icon(Icons.person),
+                    
+                    border: OutlineInputBorder(
+                      
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                    labelText: 'Email',
+                    hintText: 'Enter email',
+                    
+                  ),
+                  validator: (value) {
+                    if (value == "") {
+                      return "email is not empty";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: TextFormField(
+                  controller: password,
+                  obscureText: obs,
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      contentPadding:
+                          const EdgeInsets.symmetric(vertical: 15.0),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      labelText: 'Password',
+                      hintText: 'Enter password',
+                      suffixIcon: IconButton(
+                        icon: obs
+                            ? const Icon(Icons.visibility_off_rounded)
+                            : const Icon(Icons.remove_red_eye),
+                        onPressed: () {
+                          setState(() {
+                            obs = !obs;
+                          });
+                        },
+                      )),
+                  validator: (value) {
+                    if (value == "") {
+                      return "Password is not empty";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    txt,
+                    style: TextStyle(color: Colors.red, fontSize: 15),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(shape: const CircleBorder()),
+                onPressed: () {
+                  // if (form_key.currentState!.validate()) {
+                  //   if (username.text == password.text) {
+                  //     Navigator.popUntil(context, (route) => route.isFirst);
+                  //     Navigator.pushNamed(context, '/home');
+                  //   } else {
+                  //     txt = "Sign In failed";
+                  //   }
+                  // }
+                  //   if (form_key.currentState != null &&
+                  //       form_key.currentState!.validate()) {
+                  //     try {
+                  //       FirebaseAuth.instance
+                  //           signInWithEmailAndPassword(
+                  //               email: email.text, password: password.text)
+                  //           .then((value) {
+                  //         SignIn.pass = password.text;
+                  //         Navigator.pushNamed(context, '/home');
+                  //       }).onError((error, stackTrace) {
+                  //         print("Error ${error.toString()}");
+                  //       });
+                  //     } catch (e) {
+                  //       print("Error");
+                  //     }
+                  //   } else {
+                  //     setState(() {
+                  //       txt = "Please fill in all the fields";
+                  //     });
+                  //   }
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black,
+                          spreadRadius: 0.3,
+                          blurRadius: 6,
+                        )
+                      ]),
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 15,
+                  child: const Center(
+                    child: Text(
+                      "Đăng nhập",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 30, top: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    InkWell(
+                        onTap: () {},
+                        child: const Text(
+                          "Quên mật khẩu",
+                          style: TextStyle(
+                              color: Colors.redAccent,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        )),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "No account? ",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.popUntil(context, (route) => route.isFirst);
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            color: Colors.redAccent,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
