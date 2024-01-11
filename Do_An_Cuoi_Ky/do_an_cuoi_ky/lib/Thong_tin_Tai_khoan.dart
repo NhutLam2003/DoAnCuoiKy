823d0254c12a4fd8333528d615a3e05a168c563d
HuynhThienTam <huynhtam20202003@gmail.com>
2024-01-11 13:31:55 +0700

    trang_thong_tin_tai_khoan

---
+++ Do_An_Cuoi_Ky/do_an_cuoi_ky/lib/Thong_tin_Tai_khoan.dart
@@ -0,0 +1,224 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class TaiKhoan extends StatefulWidget {
+  const TaiKhoan({super.key});
+
+  @override
+  State<TaiKhoan> createState() => _MenuState();
+}
+
+class _MenuState extends State<TaiKhoan> {
+  // var username = FirebaseAuth.instance.currentUser?.displayName;
+  //Tâm Ngu
+  var changeUsername = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: ListView(
+      children: [
+        SizedBox(
+          width: 230,
+          height: 230,
+          child: DrawerHeader(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  backgroundImage: AssetImage('assets/user.jpg'),
+                  radius: 50,
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        barrierDismissible: false,
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text(
+                              'Edit username',
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            ),
+                            content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.fromLTRB(15, 30, 15, 20),
+                                    child: TextField(
+                                      controller: changeUsername,
+                                      decoration: const InputDecoration(
+                                          label: Text('Username'),
+                                          border: OutlineInputBorder(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(5)))),
+                                    ),
+                                  )
+                                ]),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('No')),
+                              TextButton(
+                                  style: const ButtonStyle(),
+                                  onPressed: () async {
+                                    // User? user =
+                                    //     FirebaseAuth.instance.currentUser;
+
+                                    // Kiểm tra xem người dùng có tồn tại không
+                                    // if (user != null) {
+                                    //   // Cập nhật display name
+                                    //   await user.updateDisplayName(
+                                    //       changeUsername.text);
+                                    //   // final DatabaseReference
+                                    //   //     databaseReference = FirebaseDatabase
+                                    //   //         .instance
+                                    //   //         .ref()
+                                    //   //         .child('/users');
+
+                                    //   // databaseReference
+                                    //   //
+                                    //   //     .update({
+                                    //   //   user.displayName!: changeUsername.text
+                                    //   // });
+                                    //   // In thông báo sau khi cập nhật thành công
+                                    //   print(
+                                    //       'Display Name updated successfully to: ${.displayName}');
+                                    // } else {
+                                    //   print('User is null');
+                                    // }
+                                    Navigator.popAndPushNamed(context, '/home');
+                                    setState(() {
+                                      // username = changeUsername.text;
+                                    });
+                                  },
+                                  child: const Text('Yes')),
+                            ],
+                          );
+                        });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                            //username.toString()
+                            "TAM HUYNH",
+                            style: const TextStyle(
+                                fontSize: 28, fontWeight: FontWeight.bold)),
+                        Icon(Icons.edit),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        ListTile(
+            title: const Text("Change Password"),
+            leading: const Icon(Icons.edit),
+            onTap: () {
+              Navigator.popUntil(context, (route) => route.isFirst);
+              Navigator.pushNamed(context, '/change-password');
+            }),
+        // ListTile(
+        //   title: Text("Log out"),
+        //   leading: Icon(Icons.logout),
+        //   onTap: () {
+        //     // FirebaseAuth.instance.signOut().then((value) {
+        //     //   Navigator.pushNamed(context, '/signin');
+        //     // });
+        //   },
+        // )
+        Container(
+          height: 100,
+          child: Row(
+            children: [
+              Expanded(
+                  child: TextButton(
+                      onPressed: () {},
+                      child: const Row(
+                        children: [
+                          Icon(
+                            Icons.grid_view_rounded,
+                            size: 25,
+                            color: Colors.blueGrey,
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            "Tiện ích",
+                            style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.black,
+                            ),
+                          )
+                        ],
+                      )))
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                TextButton(
+                    onPressed: () {},
+                    child: const Row(
+                      children: [
+                        Icon(
+                          Icons.access_time,
+                          size: 25,
+                          color: Colors.blueGrey,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "Đọc gần đây",
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                        )
+                      ],
+                    ))
+              ]),
+            ],
+          ),
+        ),
+
+        Container(
+          height: 100,
+          child: Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Icons.logout_outlined,
+                        size: 25,
+                        color: Colors.blueGrey,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "Đăng Xuất",
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+}
--- Do_An_Cuoi_Ky/do_an_cuoi_ky/lib/main.dart
+++ Do_An_Cuoi_Ky/do_an_cuoi_ky/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:do_an_cuoi_ky/SignIn_SignUp_ChangePW/SignIn.dart';
+import 'package:do_an_cuoi_ky/SignIn_SignUp_ChangePW/Dang_nhap.dart';
+import 'package:do_an_cuoi_ky/Thong_tin_Tai_khoan.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,25 +8,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const SignIn(),
+      home: const TaiKhoan(),
     );
   }
 }
