6978571afa97a06c71e5bf3b5728e50b3d895c65
Nhựt Lâm <146632959+NhutLam2003@users.noreply.github.com>
2024-01-18 12:17:14 +0700

    Add files via upload
    Lam thay doi chuc nang và giao dien

---
+++ DoAnCuoiKy/newsapp/lib/man_hinh/chi_tiet__tien_ich_tt/tt_API.dart
@@ -0,0 +1,16 @@
+// thêm thư viện http: ^1.1.2
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:newsapp/man_hinh/chi_tiet__tien_ich_tt/tt_model.dart';
+class ThoiTietAPI{
+  Future <ThoiTiet>? getThoiTiet(String? location) async{
+    var endpoint = Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=$location&appid=eeaca9a0fbb6cf44b9873d75f6945c68&units=metric');
+
+    var resposne = await http.get(endpoint);
+    var body = jsonDecode(resposne.body);
+    print(ThoiTiet.fromJson(body).cityname);
+    return ThoiTiet.fromJson(body);
+
+  }
+}
